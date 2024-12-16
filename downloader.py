d31cf297b195c6d0776cc925ed6d39fbb453d69a
Paula Joyce Ucol <peaksenut@gmail.com>
2024-12-16 20:48:23 +0800

    loop until every episode is downloaded while checking the folder if episode has been downloaded
    - it breaks if there is no next episode
    - but it works so
    - im not fixing it

--- downloader.py
+++ downloader.py
@@ -1,71 +1,101 @@
+import os
 import time
 import random
 import pyautogui
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.edge.service import Service
+from selenium.webdriver.edge.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 
-brave_path = r"C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"
-chromedriver_path = r"chromedriver-win64\chromedriver.exe"
+browser_path = r"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+driver_path = r"edgedriver_win64/msedgedriver.exe"
+downloads_folder = r"C:\Users\peaks\Videos\AT"
 
-options = Options()
-options.binary_location = brave_path
-options.add_argument("--start-maximized")
+def is_download_complete(folder):
+    while True:
+        files = os.listdir(folder)
+        downloading = any(file.endswith(('.crdownload', '.tmp', '.part')) for file in files)
 
-driver = webdriver.Chrome(service=Service(chromedriver_path), options=options)
+        if not downloading:
+            break
+        time.sleep(1)
 
-url = "https://www.wcostream.tv/adventure-time-season-1-episode-1-slumber-party-panic" # starts at first episode
-driver.get(url)
+# url = "https://www.wcostream.tv/adventure-time-season-1-episode-1-slumber-party-panic"  # starts at first episode
 
-# scroll down the page by  500 pixel vertical
-driver.execute_script("window.scrollTo(0, 500);")
+# test for last episode to see what happens if there is no next episode
+# if else statement below is useless -> IT BREAKS
+# url = "https://www.wcostream.tv/adventure-time-distant-lands-episode-4-wizard-city"
 
-time.sleep(1)
+while True:
+    options = Options()
+    options.add_argument("--start-maximized")
 
-episode_title_element = driver.find_element(By.XPATH, '//*[@id="content"]/table/tbody/tr/td[1]/table/tbody/tr/td/table[1]/tbody/tr[1]/td/h1/a')
-title = episode_title_element.text
-print(title)
+    driver = webdriver.Chrome(service=Service(driver_path), options=options)
 
-video_container = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, 'iframe#frameNewcizgifilmuploads0'))
-)
-video_container.click()
-time.sleep(random.uniform(4, 7))
+    try:
+        driver.get(url)
 
-driver.switch_to.frame(video_container)
+        # scroll down the page by  500 pixel vertical
+        driver.execute_script("window.scrollTo(0, 500);")
 
-episode = WebDriverWait(driver, 20).until(
-    EC.presence_of_element_located((By.TAG_NAME, "video"))
-)
+        time.sleep(1)
 
-# right click
-actions = ActionChains(driver)
-actions.move_to_element(episode).context_click().perform()
-time.sleep(random.uniform(1, 2))
+        episode_title_element = driver.find_element(By.XPATH, '//*[@id="content"]/table/tbody/tr/td[1]/table/tbody/tr/td/table[1]/tbody/tr[1]/td/h1/a')
+        title = episode_title_element.text
+        print(title)
 
-for _ in range(5):
-    # go to Save as... option
-    pyautogui.press('down')
+        video_container = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, 'iframe#frameNewcizgifilmuploads0'))
+        )
+        video_container.click()
+        time.sleep(random.uniform(60, 70))
 
-# press enter when Save as... option is selected
-pyautogui.press('enter')
+        driver.switch_to.frame(video_container)
 
-# wait cos my human eyes are not following
-time.sleep(random.uniform(5, 7))
+        episode = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.TAG_NAME, "video"))
+        )
 
-pyautogui.press('backspace')
-pyautogui.write(title)
+        # right click
+        actions = ActionChains(driver)
+        actions.move_to_element(episode).context_click().perform()
+        time.sleep(random.uniform(1, 3))
 
-# download
-pyautogui.press('enter')
+        for _ in range(4):
+            # go to Save as... option
+            pyautogui.press('down')
 
-time.sleep(random.uniform(2, 6))
+        # press enter when Save as... option is selected
+        pyautogui.press('enter')
 
-# random numbers to wait for download to finish
-time.sleep(random.uniform(480, 620))
+        # wait cos my human eyes are not following
+        time.sleep(random.uniform(1, 3))
 
-driver.quit()
+        pyautogui.press('backspace')
+        pyautogui.write(title)
+
+        # download
+        pyautogui.press('enter')
+
+        print("Waiting for download to complete...")
+        is_download_complete(downloads_folder)
+        print("Download complete!")
+
+        driver.switch_to.parent_frame()
+
+        next_episode = driver.find_elements(By.CSS_SELECTOR, "tbody tr:nth-of-type(2) td:nth-of-type(2) a")
+
+        if next_episode:
+            url = next_episode[0].get_attribute('href')
+            print(f"Next ep: {url}")
+
+            driver.quit()
+        else:
+            print(f"No Next Episode.")
+            break
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        break
